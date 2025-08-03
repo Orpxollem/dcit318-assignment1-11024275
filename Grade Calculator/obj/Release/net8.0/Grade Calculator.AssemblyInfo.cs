e3bc8531665efa6cefe064b4b5f4e0de99c5e059
Orpxollem <56360922+Orpxollem@users.noreply.github.com>
2025-08-03 23:41:50 +0000

    Triangle Type Identifier

--- Grade Calculator/obj/Release/net8.0/Grade Calculator.AssemblyInfo.cs
+++ Grade Calculator/obj/Release/net8.0/Grade Calculator.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grade Calculator")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ce40c5384e1147a6875a5fb42d80888b22144e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grade Calculator")]
